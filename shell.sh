9dfc014a83931c05863109ac92b4aace07408997
Josh Vanderberg <josh@yesenergy.com>
2022-01-31 01:50:34 +0000

    Readme and mapping in host home

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export $(cat config.env | xargs)
 echo 'Launching' $username '...'
-docker run -v ${PWD}/backup:/backup -v ${username}_home:/home/${username} -v /var/run/docker.sock:/var/run/docker.sock -ti -w /home/${username} ${username}/ubuntu:20.04 fish
+docker run -v ~:/home -v ${PWD}/backup:/backup -v ${username}_home:/home/${username} -v /var/run/docker.sock:/var/run/docker.sock -ti -w /home/${username} ${username}/ubuntu:20.04 fish
